67bc28d51f576f22a757537b0e8fb93d2e30ce98
Facundo <curtifacundomartin@gmail.com>
2019-04-25 13:58:33 -0300

    Update SQL Script

--- sql/startDB.sql
+++ sql/startDB.sql
@@ -8,11 +8,10 @@ description_rol VARCHAR(50) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS user(
 id_user INT auto_increment PRIMARY KEY NOT NULL,
-nick_user VARCHAR(50)
+nick_user VARCHAR(50),
 name_user VARCHAR(50) NOT NULL,
 lastname_user VARCHAR(50) NOT NULL,
 email_user VARCHAR(100) NOT NULL,
 pass_user VARCHAR(40) NOT NULL,
 rol_id INT NOT NULL,
-CONSTRAINT user_id_rol FOREIGN KEY (rol_id) REFERENCES rol (id_rol)
-);
+CONSTRAINT user_id_rol FOREIGN KEY (rol_id) REFERENCES rol (id_rol));
